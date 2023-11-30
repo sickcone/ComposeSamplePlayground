1b335b1647e7fc62748f7bae50bcc879f33b6b68
Sahibdeep Singh <sahibdeep.singh@nykaa.com>
2023-12-01 01:39:13 +0530

    01/12/2023 - 15 minutes of learning //Basic Text properties, modifier and it's properties

--- app/src/main/java/com/zxcv/composesampleplayground/MainActivity.kt
+++ app/src/main/java/com/zxcv/composesampleplayground/MainActivity.kt
@@ -3,13 +3,22 @@ package com.zxcv.composesampleplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.zxcv.composesampleplayground.ui.theme.ComposeSamplePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,10 +26,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeSamplePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
@@ -34,6 +41,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+            .border(5.dp, color = Color.Yellow)
+            .background(color = Color.Gray)
+            .padding(26.dp)
+            .fillMaxWidth(1f)
+            .wrapContentHeight(),
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Cyan,
+        textAlign = TextAlign.Center,
     )
 }
 
